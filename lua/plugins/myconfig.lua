243db75fc472e55a27f228e36d6a981aea272567
Dmitriy <d.lysov@tool-kit.tech>
2024-03-15 15:57:50 +0100

    upd

--- lua/plugins/myconfig.lua
+++ lua/plugins/myconfig.lua
@@ -21,27 +21,38 @@ if true then return {
       adapters = {
         "neotest-plenary",
         "olimorris/neotest-phpunit",
-      }
+      },
     },
   },
 
-{
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
   },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+
+  {
+    "andythigpen/nvim-coverage",
+    requires = "nvim-lua/plenary.nvim",
+    -- Optional: needed for PHP when using the cobertura parser
+    rocks = { 'lua-xmlreader' },
+    config = function()
+      require("coverage").setup()
+    end,
   },
-}
+
 } end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
